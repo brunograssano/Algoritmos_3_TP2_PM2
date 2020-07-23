0812d99a9b2afc62ad222d59755c9719505d0565
brunograssano <grassanobruno@gmail.com>
2020-07-23 15:46:52 -0300

    Test: "Agregamos unos tests en la clase jugador"
    Co-authored-by: Joaquin Gomez <joaqogomez@gmail.com>

--- src/main/test/edu/fiuba/algo3/modelo/JugadorTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/JugadorTest.java
@@ -106,5 +106,104 @@ public class JugadorTest {
 
     }
 
+    @Test
+    public void test05UnJugadorRespondeErroneamenteUnaPreguntaVerdaderoFalsoPenalizableYTienePuntosNegativos(){
+        TipoPuntajePenalizable tipoPenalizable = new TipoPuntajePenalizable();
+        MetodoVerdaderoFalso verdaderoFalso = new MetodoVerdaderoFalso();
+        String enunciado = "Existe el comando git mix?";
+
+        RespuestaCorrecta respuestaCorrecta = new RespuestaCorrecta("Falso");
+        RespuestaIncorrecta respuestaIncorrecta = new RespuestaIncorrecta("Verdadero");
+
+        ArrayList<RespuestaComun> respuestasPosibles = new ArrayList<>();
+
+        respuestasPosibles.add(respuestaCorrecta);
+        respuestasPosibles.add(respuestaIncorrecta);
+
+        Pregunta unaPregunta = new Pregunta(enunciado,respuestasPosibles,verdaderoFalso,tipoPenalizable);
+
+        Jugador jugador = new Jugador("Lucas");
+
+        RespuestaComun respuestaJugador = respuestaIncorrecta;
+        ArrayList<RespuestaComun> respuestasJugador = new ArrayList<>();
+        respuestasJugador.add(respuestaJugador);
+
+        jugador.responder(unaPregunta,respuestasJugador);
+
+        assertEquals(-1,jugador.obtenerPuntos());
+
+    }
+
+
+    @Test
+    public void test06UnJugadorRespondeCorrectamenteUnaPreguntaVerdaderoFalsoPenalizableYSumaPuntos(){
+        TipoPuntajePenalizable tipoPenalizable = new TipoPuntajePenalizable();
+        MetodoVerdaderoFalso verdaderoFalso = new MetodoVerdaderoFalso();
+        String enunciado = "Existe el comando git mix?";
+
+        RespuestaCorrecta respuestaCorrecta = new RespuestaCorrecta("Falso");
+        RespuestaIncorrecta respuestaIncorrecta = new RespuestaIncorrecta("Verdadero");
+
+        ArrayList<RespuestaComun> respuestasPosibles = new ArrayList<>();
+
+        respuestasPosibles.add(respuestaCorrecta);
+        respuestasPosibles.add(respuestaIncorrecta);
+
+        Pregunta unaPregunta = new Pregunta(enunciado,respuestasPosibles,verdaderoFalso,tipoPenalizable);
+
+        Jugador jugador = new Jugador("Lucas");
+
+        RespuestaComun respuestaJugador = respuestaCorrecta;
+        ArrayList<RespuestaComun> respuestasJugador = new ArrayList<>();
+        respuestasJugador.add(respuestaJugador);
+
+        jugador.responder(unaPregunta,respuestasJugador);
+
+        assertEquals(1,jugador.obtenerPuntos());
+
+    }
+
+    @Test
+    public void test07UnJugadorRespondeVariasPreguntasVFPenalizablesYLeQuedan0Puntos(){
+        TipoPuntajePenalizable tipoPenalizable = new TipoPuntajePenalizable();
+        MetodoVerdaderoFalso verdaderoFalso = new MetodoVerdaderoFalso();
+        String enunciado = "Todo metodo iterativo se puede hacer recursivo?";
+
+        RespuestaCorrecta respuestaCorrecta = new RespuestaCorrecta("Verdadero");
+        RespuestaIncorrecta respuestaIncorrecta = new RespuestaIncorrecta("Falso");
+
+        ArrayList<RespuestaComun> respuestasPosibles = new ArrayList<>();
+
+        respuestasPosibles.add(respuestaCorrecta);
+        respuestasPosibles.add(respuestaIncorrecta);
+
+        Pregunta unaPregunta1 = new Pregunta(enunciado,respuestasPosibles,verdaderoFalso,tipoPenalizable);
+        Pregunta unaPregunta2 = new Pregunta(enunciado,respuestasPosibles,verdaderoFalso,tipoPenalizable);
+        Pregunta unaPregunta3 = new Pregunta(enunciado,respuestasPosibles,verdaderoFalso,tipoPenalizable);
+        Pregunta unaPregunta4 = new Pregunta(enunciado,respuestasPosibles,verdaderoFalso,tipoPenalizable);
+        Pregunta unaPregunta5 = new Pregunta(enunciado,respuestasPosibles,verdaderoFalso,tipoPenalizable);
+        Pregunta unaPregunta6 = new Pregunta(enunciado,respuestasPosibles,verdaderoFalso,tipoPenalizable);
+
+        Jugador jugador = new Jugador("Lucas");
+        RespuestaComun respuestaCorrectaJugador = respuestaCorrecta;
+        RespuestaComun respuestaIncorrectaJugador = respuestaIncorrecta;
+
+        ArrayList<RespuestaComun> respuestasJugador = new ArrayList<>();
+        respuestasJugador.add(respuestaCorrectaJugador);
+
+        jugador.responder(unaPregunta1,respuestasJugador);
+        jugador.responder(unaPregunta2,respuestasJugador);
+        jugador.responder(unaPregunta3,respuestasJugador);
+
+        respuestasJugador.remove(respuestaCorrectaJugador);
+        respuestasJugador.add(respuestaIncorrectaJugador);
+
+        jugador.responder(unaPregunta4,respuestasJugador);
+        jugador.responder(unaPregunta5,respuestasJugador);
+        jugador.responder(unaPregunta6,respuestasJugador);
+
+        assertEquals(0,jugador.obtenerPuntos());
+
+    }
 
 }
