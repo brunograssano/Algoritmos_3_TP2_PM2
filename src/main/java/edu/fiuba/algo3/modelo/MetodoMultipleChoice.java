62004412eaee046db37f641688172a10fd55ee4d
Guillermo Stancanelli <guillermo_stancanelli@outlook.com>
2020-07-23 18:57:05 -0300

    clase base del MetodoMultipleChoice, a ser cambiada por las recientes modificaciones en la evaluacion de respuestas
    Co-authored-by: Juan Roussilian <juancruzroussilian9@gmail.com>

--- src/main/java/edu/fiuba/algo3/modelo/MetodoMultipleChoice.java
+++ src/main/java/edu/fiuba/algo3/modelo/MetodoMultipleChoice.java
@@ -1,4 +1,62 @@
 package edu.fiuba.algo3.modelo;
 
-public class MetodoMultipleChoice {
+import edu.fiuba.algo3.Excepciones.CantidadErroneaDeRespuestasParaPreguntaException;
+
+import java.util.ArrayList;
+
+public class MetodoMultipleChoice implements MetodoRespuesta {
+
+    static final int CANT_OPCIONES_MIN = 2;
+    static final int CANT_OPCIONES_MAX = 5;
+
+    private TipoPuntaje tipoPuntaje;
+    private ArrayList<RespuestaComun> respuestas;
+
+    @Override
+    public void agregarRespuestas(ArrayList<RespuestaComun> respuestas) {
+        if(respuestas.size() < CANT_OPCIONES_MIN || respuestas.size() > CANT_OPCIONES_MAX){
+            throw new CantidadErroneaDeRespuestasParaPreguntaException();
+        }
+        this.respuestas = respuestas;
+    }
+
+    @Override
+    public void agregarTipo(TipoPuntaje unTipoPuntos) {
+        this.tipoPuntaje = unTipoPuntos;
+    }
+
+    @Override
+    public int evaluarClasico(ArrayList<RespuestaComun> respuestasJugador) {
+        if(respuestas.size() < CANT_OPCIONES_MIN || respuestas.size() > CANT_OPCIONES_MAX){
+            throw new CantidadErroneaDeRespuestasParaPreguntaException();
+        }
+        int puntos = 0;
+
+        return puntos;
+    }
+
+    @Override
+    public int evaluarPenalizable(ArrayList<RespuestaComun> respuestas) {
+        if(respuestas.size() < CANT_OPCIONES_MIN || respuestas.size() > CANT_OPCIONES_MAX) {
+            throw new CantidadErroneaDeRespuestasParaPreguntaException();
+        }
+        int puntos = 0;
+        for (RespuestaComun respuesta: respuestas){
+            puntos += respuesta.evaluarPenalizable();
+        }
+        return puntos;
+    }
+// no hago override porque MetodoRespuesta aun no tiene el evaluarParcial. Hay que considerar hacerla una interfaz aparte para no joder VoF.
+    public int evaluarParcial(ArrayList<RespuestaComun> respuestas) {
+        if(respuestas.size() < CANT_OPCIONES_MIN || respuestas.size() > CANT_OPCIONES_MAX) {
+            throw new CantidadErroneaDeRespuestasParaPreguntaException();
+        }
+        int puntos = 0;
+        for (RespuestaComun respuesta: respuestas){
+            puntos += respuesta.evaluarClasico();
+        }
+        return puntos;
+    }
+
 }
+
