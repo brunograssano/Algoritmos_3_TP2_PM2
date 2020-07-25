ed52ff9a9251454607d299bb070ed82803951cbc
Juan Cruz <juancruzroussilian9@gmail.com>
2020-07-25 01:16:47 -0300

    Termino las pruebas unitarias de MetodoChoice y agrego minimo de respuestas correctas en el modelo

--- src/main/java/edu/fiuba/algo3/modelo/MetodoMultipleChoice.java
+++ src/main/java/edu/fiuba/algo3/modelo/MetodoMultipleChoice.java
@@ -8,6 +8,7 @@ public class MetodoMultipleChoice implements MetodoRespuesta {
 
     static final int CANT_OPCIONES_MIN = 2;
     static final int CANT_OPCIONES_MAX = 5;
+    static final int RESPUESTAS_CORRECTAS_MIN = 1;
 
     private int respuestasTotales = 0;
     private TipoPuntaje tipoPuntaje;
@@ -18,7 +19,7 @@ public class MetodoMultipleChoice implements MetodoRespuesta {
     public void agregarRespuestas(ArrayList<RespuestaCorrecta> respuestasCorrectas,ArrayList<RespuestaIncorrecta> respuestasIncorrectas ) {
 
         respuestasTotales = respuestasCorrectas.size() + respuestasIncorrectas.size();
-        if(respuestasTotales < CANT_OPCIONES_MIN || respuestasTotales > CANT_OPCIONES_MAX){
+        if(respuestasTotales < CANT_OPCIONES_MIN || respuestasTotales > CANT_OPCIONES_MAX || respuestasCorrectas.size() < RESPUESTAS_CORRECTAS_MIN ) {
             throw new CantidadErroneaDeRespuestasParaPreguntaException();
         }
         this.respuestasCorrectas = respuestasCorrectas;
--- src/main/test/edu/fiuba/algo3/modelo/PreguntasMultipleChoiceTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/PreguntasMultipleChoiceTest.java
@@ -1,10 +1,12 @@
 package edu.fiuba.algo3.modelo;
 
+import edu.fiuba.algo3.Excepciones.CantidadErroneaDeRespuestasParaPreguntaException;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PreguntasMultipleChoiceTest {
 
@@ -67,7 +69,26 @@ public class PreguntasMultipleChoiceTest {
         assertEquals(0,puntos);
     }
     @Test
-    public void test03CreoUnaPreguntaMultipleChoiceParcialSeEligenTresCorrectasYDevuelveTresPuntos(){
+    public void test03IntentoCrearUnaPreguntaMultipleChoiceCon1RespuestaYLanzaExcepcion(){
+
+        TipoPuntajeParcial tipoParcial = new TipoPuntajeParcial();
+        MetodoMultipleChoice multipleChoice = new MetodoMultipleChoice();
+        String enunciado = "Esta pregunta es una prueba?";
+        RespuestaCorrecta respuestaCorrecta = new RespuestaCorrecta("Nah");
+        
+        ArrayList<RespuestaCorrecta> respuestasCorrectas = new ArrayList<>();
+        ArrayList<RespuestaIncorrecta> respuestasIncorrectas = new ArrayList<>();
+
+        respuestasCorrectas.add(respuestaCorrecta);
+
+
+        assertThrows(CantidadErroneaDeRespuestasParaPreguntaException.class,
+                ()->{Pregunta unaPregunta = new Pregunta(enunciado, respuestasCorrectas, respuestasIncorrectas, multipleChoice ,tipoParcial);}
+        );
+    }
+
+    @Test
+    public void test04CreoUnaPreguntaMultipleChoiceParcialSeEligenTresCorrectasYDevuelveTresPuntos(){
 
         TipoPuntajeParcial tipoParcial = new TipoPuntajeParcial();
         MetodoMultipleChoice multipleChoice = new MetodoMultipleChoice();
@@ -96,7 +117,7 @@ public class PreguntasMultipleChoiceTest {
         assertEquals(3,puntos);
     }
     @Test
-    public void test04CreoUnaPreguntaMultipleChoiceParcialSeEligenDosCorrectasYDevuelveDosPuntos(){
+    public void test05CreoUnaPreguntaMultipleChoiceParcialSeEligenDosCorrectasYDevuelveDosPuntos(){
 
         TipoPuntajeParcial tipoParcial = new TipoPuntajeParcial();
         MetodoMultipleChoice multipleChoice = new MetodoMultipleChoice();
@@ -124,7 +145,7 @@ public class PreguntasMultipleChoiceTest {
         assertEquals(2,puntos);
     }
     @Test
-    public void test04CreoUnaPreguntaMultipleChoiceParcialSeEligenDosCorrectasYDosIncorrectasYNoDevuelvePuntos(){
+    public void test06CreoUnaPreguntaMultipleChoiceParcialSeEligenDosCorrectasYDosIncorrectasYNoDevuelvePuntos(){
 
         TipoPuntajeParcial tipoParcial = new TipoPuntajeParcial();
         MetodoMultipleChoice multipleChoice = new MetodoMultipleChoice();
@@ -152,5 +173,165 @@ public class PreguntasMultipleChoiceTest {
         int puntos = unaPregunta.responder(respuestasJugador);
 
         assertEquals(0,puntos);
+    }
+
+    @Test
+    public void test07CreoUnaPreguntaMultipleChoiceParcialYSeEligen4RespuestasCorrectasYUnaIncorrectaYNoDevuelvePuntos() {
+
+        TipoPuntajeParcial tipoParcial = new TipoPuntajeParcial();
+        MetodoMultipleChoice multipleChoice = new MetodoMultipleChoice();
+        String enunciado = "Cuales de estos son pokemones?";
+
+        RespuestaCorrecta respuestaCorrecta1 = new RespuestaCorrecta("Pikachu");
+        RespuestaCorrecta respuestaCorrecta2 = new RespuestaCorrecta("Giratina");
+        RespuestaCorrecta respuestaCorrecta3 = new RespuestaCorrecta("Charizard");
+        RespuestaCorrecta respuestaCorrecta4 = new RespuestaCorrecta("Entei");
+        RespuestaIncorrecta respuestaIncorrecta1 = new RespuestaIncorrecta("Gelatina");
+
+        ArrayList<RespuestaCorrecta> respuestasCorrectas = new ArrayList<>();
+        ArrayList<RespuestaIncorrecta> respuestasIncorrectas = new ArrayList<>();
+
+        respuestasCorrectas.add(respuestaCorrecta1);
+        respuestasCorrectas.add(respuestaCorrecta2);
+        respuestasCorrectas.add(respuestaCorrecta3);
+        respuestasCorrectas.add(respuestaCorrecta4);
+        respuestasIncorrectas.add(respuestaIncorrecta1);
+
+        Pregunta unaPregunta = new Pregunta(enunciado,respuestasCorrectas,respuestasIncorrectas,multipleChoice,tipoParcial);
+        ArrayList<RespuestaComun> respuestasJugador = new ArrayList<>();
+        respuestasJugador.add(respuestaCorrecta1);
+        respuestasJugador.add(respuestaCorrecta2);
+        respuestasJugador.add(respuestaCorrecta3);
+        respuestasJugador.add(respuestaCorrecta4);
+        respuestasJugador.add(respuestaIncorrecta1);
+
+        int puntos = unaPregunta.responder(respuestasJugador);
+
+        assertEquals(0,puntos);
+    }
+
+    @Test
+    public void test08CreoUnaPreguntaMultipleChoicePenalizableYSeEligen3RespuestasCorrectasYDevuelve3Puntos() {
+
+        TipoPuntajePenalizable  puntaje = new TipoPuntajePenalizable();
+        MetodoMultipleChoice multipleChoice = new MetodoMultipleChoice();
+        String enunciado = "Cuales de estos son chocolates?";
+
+        RespuestaCorrecta respuestaCorrecta1 = new RespuestaCorrecta("Milka");
+        RespuestaCorrecta respuestaCorrecta2 = new RespuestaCorrecta("Cardbury");
+        RespuestaCorrecta respuestaCorrecta3 = new RespuestaCorrecta("Block");
+
+        ArrayList<RespuestaCorrecta> respuestasCorrectas = new ArrayList<>();
+        ArrayList<RespuestaIncorrecta> respuestasIncorrectas = new ArrayList<>();
+
+        respuestasCorrectas.add(respuestaCorrecta1);
+        respuestasCorrectas.add(respuestaCorrecta2);
+        respuestasCorrectas.add(respuestaCorrecta3);
+
+        Pregunta unaPregunta = new Pregunta(enunciado,respuestasCorrectas,respuestasIncorrectas,multipleChoice,puntaje);
+        ArrayList<RespuestaComun> respuestasJugador = new ArrayList<>();
+        respuestasJugador.add(respuestaCorrecta1);
+        respuestasJugador.add(respuestaCorrecta2);
+        respuestasJugador.add(respuestaCorrecta3);
+
+        int puntos = unaPregunta.responder(respuestasJugador);
+
+        assertEquals(3,puntos);
+    }
+
+    @Test
+    public void test09CreoUnaPreguntaMultipleChoicePenalizableYSeEligenRespuestas2IncorrectasYDevuelve2PuntosNegativos() {
+
+        TipoPuntajePenalizable  puntaje = new TipoPuntajePenalizable();
+        MetodoMultipleChoice multipleChoice = new MetodoMultipleChoice();
+        String enunciado = "Cuales de estos son chocolates?";
+
+        RespuestaIncorrecta respuestaIncorrecta1 = new RespuestaIncorrecta("Doritos");
+        RespuestaIncorrecta respuestaIncorrecta2 = new RespuestaIncorrecta("Broccoli");
+        RespuestaCorrecta respuestaCorrecta1 = new RespuestaCorrecta("Milka");
+
+        ArrayList<RespuestaCorrecta> respuestasCorrectas = new ArrayList<>();
+        ArrayList<RespuestaIncorrecta> respuestasIncorrectas = new ArrayList<>();
+        respuestasCorrectas.add(respuestaCorrecta1);
+        respuestasIncorrectas.add(respuestaIncorrecta1);
+        respuestasIncorrectas.add(respuestaIncorrecta2);
+
+        Pregunta unaPregunta = new Pregunta(enunciado,respuestasCorrectas,respuestasIncorrectas,multipleChoice,puntaje);
+        ArrayList<RespuestaComun> respuestasJugador = new ArrayList<>();
+        respuestasJugador.add(respuestaIncorrecta1);
+        respuestasJugador.add(respuestaIncorrecta2);
+
+        int puntos = unaPregunta.responder(respuestasJugador);
+
+        assertEquals(-2,puntos);
+    }
+
+    @Test
+    public void test10CreoUnaPreguntaMultipleChoicePenalizableYSeEligen3RespuestasCorrectasY2IncorrectaYDevuelve1Punto() {
+
+        TipoPuntajePenalizable  puntaje = new TipoPuntajePenalizable();
+        MetodoMultipleChoice multipleChoice = new MetodoMultipleChoice();
+        String enunciado = "Cuales de estos son chocolates?";
+
+        RespuestaCorrecta respuestaCorrecta1 = new RespuestaCorrecta("Milka");
+        RespuestaCorrecta respuestaCorrecta2 = new RespuestaCorrecta("Block");
+        RespuestaCorrecta respuestaCorrecta3 = new RespuestaCorrecta("Shot");
+        RespuestaIncorrecta respuestaIncorrecta1 = new RespuestaIncorrecta("Doritos");
+        RespuestaIncorrecta respuestaIncorrecta2 = new RespuestaIncorrecta("Broccoli");
+
+        ArrayList<RespuestaCorrecta> respuestasCorrectas = new ArrayList<>();
+        ArrayList<RespuestaIncorrecta> respuestasIncorrectas = new ArrayList<>();
+        respuestasCorrectas.add(respuestaCorrecta1);
+        respuestasCorrectas.add(respuestaCorrecta2);
+        respuestasCorrectas.add(respuestaCorrecta3);
+        respuestasIncorrectas.add(respuestaIncorrecta1);
+        respuestasIncorrectas.add(respuestaIncorrecta2);
+
+        Pregunta unaPregunta = new Pregunta(enunciado,respuestasCorrectas,respuestasIncorrectas,multipleChoice,puntaje);
+        ArrayList<RespuestaComun> respuestasJugador = new ArrayList<>();
+
+        respuestasJugador.add(respuestaCorrecta1);
+        respuestasJugador.add(respuestaCorrecta2);
+        respuestasJugador.add(respuestaCorrecta3);
+        respuestasJugador.add(respuestaIncorrecta1);
+        respuestasJugador.add(respuestaIncorrecta2);
+
+        int puntos = unaPregunta.responder(respuestasJugador);
+
+        assertEquals(1,puntos);
+    }
+    @Test
+    public void test11CreoUnaPreguntaMultipleChoicePenalizableYSeEligen2RespuestasCorrectasY3IncorrectasYDevuelve1PuntoNegativo() {
+
+        TipoPuntajePenalizable  puntaje = new TipoPuntajePenalizable();
+        MetodoMultipleChoice multipleChoice = new MetodoMultipleChoice();
+        String enunciado = "Cuales de estos son lenguajes de programacion?";
+
+        RespuestaCorrecta respuestaCorrecta1 = new RespuestaCorrecta("C#");
+        RespuestaCorrecta respuestaCorrecta2 = new RespuestaCorrecta("PHP");
+        RespuestaIncorrecta respuestaIncorrecta1 = new RespuestaIncorrecta("HTML");
+        RespuestaIncorrecta respuestaIncorrecta2 = new RespuestaIncorrecta("FFFX");
+        RespuestaIncorrecta respuestaIncorrecta3 = new RespuestaIncorrecta("CSS");
+
+        ArrayList<RespuestaCorrecta> respuestasCorrectas = new ArrayList<>();
+        ArrayList<RespuestaIncorrecta> respuestasIncorrectas = new ArrayList<>();
+        respuestasCorrectas.add(respuestaCorrecta1);
+        respuestasCorrectas.add(respuestaCorrecta2);
+        respuestasIncorrectas.add(respuestaIncorrecta1);
+        respuestasIncorrectas.add(respuestaIncorrecta2);
+        respuestasIncorrectas.add(respuestaIncorrecta3);
+
+        Pregunta unaPregunta = new Pregunta(enunciado,respuestasCorrectas,respuestasIncorrectas,multipleChoice,puntaje);
+        ArrayList<RespuestaComun> respuestasJugador = new ArrayList<>();
+
+        respuestasJugador.add(respuestaCorrecta1);
+        respuestasJugador.add(respuestaCorrecta2);
+        respuestasJugador.add(respuestaIncorrecta1);
+        respuestasJugador.add(respuestaIncorrecta2);
+        respuestasJugador.add(respuestaIncorrecta3);
+
+        int puntos = unaPregunta.responder(respuestasJugador);
+
+        assertEquals(-1,puntos);
     }
 }
