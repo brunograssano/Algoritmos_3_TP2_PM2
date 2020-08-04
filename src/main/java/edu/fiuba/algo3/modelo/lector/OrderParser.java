412dabc65dda31ee257efbc6be6907b05007c787
Guillermo Stancanelli <guillermo_stancanelli@outlook.com>
2020-08-04 00:04:25 -0300

    arreglo bug de lectura en el parser de Order

--- src/main/java/edu/fiuba/algo3/modelo/lector/OrderParser.java
+++ src/main/java/edu/fiuba/algo3/modelo/lector/OrderParser.java
@@ -2,7 +2,7 @@ package edu.fiuba.algo3.modelo.lector;
 
 import edu.fiuba.algo3.modelo.preguntas.FabricaDePreguntas;
 import edu.fiuba.algo3.modelo.preguntas.Pregunta;
-import edu.fiuba.algo3.modelo.preguntas.orderedChoice.RespuestaOrden;
+import edu.fiuba.algo3.modelo.preguntas.opciones.OpcionSimple;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
@@ -14,13 +14,13 @@ public class OrderParser implements Parser {
     @Override
     public Pregunta parse(JSONObject jsPregunta) {
 
-        JSONObject jOrder = (JSONObject) jsPregunta.get("MC");
+        JSONObject jOrder = (JSONObject) jsPregunta.get("ORDER");
         JSONArray jrespuestasEnOrden = (JSONArray) jOrder.get("respuestasEnOrden");
         String enunciado = (String) jOrder.get("enunciado");
 
-        ArrayList<RespuestaOrden> respuestasOrdenadas = new ArrayList<>();
+        ArrayList<OpcionSimple> respuestasOrdenadas = new ArrayList<>();
 
-        jrespuestasEnOrden.forEach(jsRespuesta-> respuestasOrdenadas.add(new RespuestaOrden((String) jsRespuesta )));
+        jrespuestasEnOrden.forEach(jsRespuesta-> respuestasOrdenadas.add(new OpcionSimple((String) jsRespuesta )));
 
 //        for ( Object jsonRespuesta : jrespuestasEnOrden ){
 //            respuestasOrdenadas.add(new RespuestaOrden((String) jsonRespuesta));
