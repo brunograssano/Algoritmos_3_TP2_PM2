45abe4dad42c793e67867764de7122db1185d44f
Juan Cruz <juancruzroussilian9@gmail.com>
2020-08-06 20:21:04 -0300

    Aproximacion inicial a la interfaz inicial
    Co-authored-by: Bruno Grassano <bgrassano@fi.uba.ar>
    Co-authored-by: Joaquin Gomez <joaqogomez@gmail.com>
    Co-authored-by: Guillermo Stancanelli <guillermo_stancanelli@outlook.com>

--- src/main/java/edu/fiuba/algo3/App.java
+++ src/main/java/edu/fiuba/algo3/App.java
@@ -1,9 +1,16 @@
 package edu.fiuba.algo3;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 /**
@@ -11,13 +18,42 @@ import javafx.stage.Stage;
  */
 public class App extends Application {
 
+    static String VIOLETA = "9370DB";
+    static String GRIS = "D8DDEF";
+    static String VERDE = "33FF96";
+    static String AZUL = "0083E0";
+    static String ROJO = "EF2D56";
+    static String AMARILLO = "FBD87F";
+
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+
+        StackPane layout = new StackPane();
+        Background fonditoGris = new Background(new BackgroundFill(Color.web(GRIS), CornerRadii.EMPTY, Insets.EMPTY));
+        layout.setBackground(fonditoGris);
+
+        VBox cajaPrincipal = new VBox(70);
+        cajaPrincipal.setAlignment(Pos.CENTER);
+
+        Label label = new Label("AlgoHoot");
+        label.setFont(Font.font("comic sans ms",80));
+        label.setTextFill(Color.web(VIOLETA));
+        cajaPrincipal.getChildren().add(label);
+
+
+        ToggleButton button = new ToggleButton();
+        button.setText("Iniciar partida :^)");
+        button.setFont(Font.font("comic sans ms", 20));
+        button.setTextFill(Color.web(GRIS));
+        Background unFondito = new Background(new BackgroundFill(Color.web(VIOLETA), new CornerRadii(9), new Insets(1)));
+        button.setBackground(unFondito);
+        cajaPrincipal.getChildren().add(button);
+
+
+        layout.getChildren().add(cajaPrincipal);
+
+        var scene = new Scene(layout, 800, 600);
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/edu/fiuba/algo3/vistas/VistaInicio.java
@@ -0,0 +1,12 @@
+package edu.fiuba.algo3.vistas;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class VistaInicio {
+
+
+}
