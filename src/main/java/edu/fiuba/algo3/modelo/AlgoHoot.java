3bb8e5f17b4b1bd4b6f253da82400098384d7b16
brunograssano <grassanobruno@gmail.com>
2020-08-08 14:15:26 -0300

    Feat: "Acercamiento a algohoot"
    Co-authored-by: Joaquin Gomez <joaqogomez@gmail.com>

---
+++ src/main/java/edu/fiuba/algo3/modelo/AlgoHoot.java
@@ -0,0 +1,63 @@
+package edu.fiuba.algo3.modelo;
+
+import edu.fiuba.algo3.modelo.modificadores.Modificador;
+import edu.fiuba.algo3.modelo.preguntas.Pregunta;
+import edu.fiuba.algo3.modelo.preguntas.respuestasJugador.Respuesta;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Stack;
+
+public class AlgoHoot {
+
+
+    private static AlgoHoot algohoot = new AlgoHoot();
+    private ArrayList<Pregunta> preguntas;
+    private Stack<Jugada> jugadas;
+    private Jugador jugador1;
+    private Jugador jugador2;
+
+
+    private AlgoHoot(){
+        jugadas = new Stack<>();
+    }
+
+    public static AlgoHoot getInstance(){
+        return algohoot;
+    }
+
+    public void agregarPreguntas(ArrayList<Pregunta> preguntas){
+        //llamada al lector, este devuelve el array
+        //verificar si esta vacio, se encarga el lector?
+        this.preguntas = preguntas;
+    }
+
+    public void agregarJugadores(String nombreJugador1,String nombreJugador2){
+        jugador1 = new Jugador(nombreJugador1);
+        jugador2 = new Jugador(nombreJugador2);
+        crearJugadas();
+    }
+
+    private void crearJugadas(){
+        Collections.shuffle(preguntas);
+        for(Pregunta pregunta:preguntas) {
+            jugadas.push(new Jugada(jugador1,jugador2,pregunta));
+        }
+    }
+
+    public Jugada pedirJugada(){
+        return jugadas.peek();
+    }
+
+    public void jugar(Respuesta respuestaJugador1, Respuesta respuestaJugador2){
+        Jugada jugadaActual = jugadas.pop();
+        jugadaActual.procesarJugada(respuestaJugador1,respuestaJugador2);
+    }
+
+    public void usarModificador(Modificador modificador){
+        Jugada jugadaActual = jugadas.peek();
+        jugadaActual.agregarModificador(modificador);
+    }
+
+}
+
