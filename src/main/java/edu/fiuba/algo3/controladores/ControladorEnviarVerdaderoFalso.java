c01682e06ad2cd40772c5fe7c50ffd3f70cb2a1a
Joaquin <joaqogomez@gmail.com>
2020-08-09 18:10:05 -0300

    Agrego controllers para que funcione

---
+++ src/main/java/edu/fiuba/algo3/controladores/ControladorEnviarVerdaderoFalso.java
@@ -0,0 +1,31 @@
+package edu.fiuba.algo3.controladores;
+
+import edu.fiuba.algo3.modelo.AlgoHoot;
+import edu.fiuba.algo3.modelo.preguntas.opciones.OpcionEvaluable;
+import edu.fiuba.algo3.modelo.preguntas.respuestasJugador.RespuestaVerdaderoFalso;
+import edu.fiuba.algo3.vistas.VistaTransicionPregunta;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class ControladorEnviarVerdaderoFalso implements EventHandler<ActionEvent> {
+
+    private OpcionEvaluable respuestasUsuario;
+    private Stage stage;
+
+    public ControladorEnviarVerdaderoFalso(Stage stage, OpcionEvaluable unaOpcion){
+        respuestasUsuario = unaOpcion;
+        this.stage = stage;
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+        RespuestaVerdaderoFalso respuestaDeUnJugador = new RespuestaVerdaderoFalso(respuestasUsuario);
+        AlgoHoot.getInstance().procesarTurno(respuestaDeUnJugador);
+        VistaTransicionPregunta vistaTransicion = new VistaTransicionPregunta(stage);
+        Scene scene = new Scene(vistaTransicion,800,600);
+        stage.setScene(scene);
+    }
+}
