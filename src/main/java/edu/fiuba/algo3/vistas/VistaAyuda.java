f67d46dabbeffcbab2dc80bedfb6ea210af8bd8a
brunograssano <grassanobruno@gmail.com>
2020-08-15 20:06:29 -0300

    "Mejoro la ventana de ayuda del juego"

--- src/main/java/edu/fiuba/algo3/vistas/VistaAyuda.java
+++ src/main/java/edu/fiuba/algo3/vistas/VistaAyuda.java
@@ -2,13 +2,14 @@ package edu.fiuba.algo3.vistas;
 
 import edu.fiuba.algo3.vistas.textos.TextoAyuda;
 import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 public class VistaAyuda extends StackPane {
     public VistaAyuda() {
-        super();
-
         Image imagen = new Image("file:"+System.getProperty("user.dir") + "/src/main/java/edu/fiuba/algo3/resources/imagenes/fondoInicio.jpg");
         BackgroundImage fondoImagen = new BackgroundImage(imagen, BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
         Background fondo = new Background(fondoImagen);
@@ -17,6 +18,12 @@ public class VistaAyuda extends StackPane {
         VBox cajaPrincipal = new VBox(70);
         cajaPrincipal.setAlignment(Pos.CENTER);
 
+        Label titulo = new Label("Ayuda sobre el juego");
+        titulo.setFont(Font.font("montserrat",25));
+        titulo.setTextFill(Color.BLACK);
+        titulo.setWrapText(true);
+
+        cajaPrincipal.getChildren().add(titulo);
         cajaPrincipal.getChildren().add(new TextoAyuda());
 
         super.getChildren().add(cajaPrincipal);
--- src/main/java/edu/fiuba/algo3/vistas/textos/TextoAyuda.java
+++ src/main/java/edu/fiuba/algo3/vistas/textos/TextoAyuda.java
@@ -6,8 +6,16 @@ import javafx.scene.text.Font;
 
 public class TextoAyuda extends Label {
     public TextoAyuda(){
-        super("El juego consiste en responder diferentes tipos de preguntas, y conseguir la mayor cantidad posible de puntos");
-        super.setFont(Font.font("montserrat",40));
+        super("El juego consiste en responder diferentes tipos de preguntas, y conseguir la mayor cantidad posible de puntos\n" +
+                "Existen 4 tipos distintos de preguntas, verdadero falso, multiple choice, orden y grupos. \n" +
+                "Tambien hay distintos tipos de puntaje, estos son clasico, parcial, y penalizable. \n" +
+                "Si es clasico, debes de responder correctamente todas las respuestas correctas para tener un punto.\n" +
+                "Si es parcial (solo multiple choice) obtenes un punto por cada correcta, 0 si te equivocas.\n" +
+                "Si es penalizable tenes un punto por cada correcta y menos un punto por cada incorrecta.\n" +
+                "Por ultimo, hay que tener en cuenta los modificadores, tenes dos multiplicadores, un x2 y un x3 " +
+                "para usar en las preguntas penalizables. Tambien tenes dos exclusividades, que al usarla obtenes el doble de puntos" +
+                " si solo un jugador respondio correctamente. (en preguntas con puntaje clasico y parcial)");
+        super.setFont(Font.font("montserrat",20));
         super.setTextFill(Color.BLACK);
         super.setWrapText(true);
     }
