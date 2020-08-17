2f8c62832fe331b6edb2f276f2c754ca8ed5cf28
Joaquin <joaqogomez@gmail.com>
2020-08-17 16:32:02 -0300

    Agregado de los circulos con bonus en el jugador

--- src/main/java/edu/fiuba/algo3/vistas/PaletaColoresHexa.java
+++ src/main/java/edu/fiuba/algo3/vistas/PaletaColoresHexa.java
@@ -1,10 +1,10 @@
 package edu.fiuba.algo3.vistas;
 
 public class PaletaColoresHexa {
-    private static String GRIS = "D8DDEF";
-    private static String VIOLETA = "9370DB";
-    private static String VERDE= "33FF96";
-    private static String AZUL = "0083E0";
-    private static String AMARILLO = "FBD87F";
-    private static String ROJO = "EF2D56";
+    public static String GRIS = "D8DDEF";
+    public static String VIOLETA = "9370DB";
+    public static String VERDE= "33FF96";
+    public static String AZUL = "0083E0";
+    public static String AMARILLO = "FBD87F";
+    public static String ROJO = "EF2D56";
 }
---
+++ src/main/java/edu/fiuba/algo3/vistas/seccionesVista/CajaBonusJugador.java
@@ -0,0 +1,37 @@
+package edu.fiuba.algo3.vistas.seccionesVista;
+
+import edu.fiuba.algo3.modelo.AlgoHoot;
+import edu.fiuba.algo3.modelo.Jugador;
+import edu.fiuba.algo3.modelo.modificadores.exclusividad.Exclusividad;
+import edu.fiuba.algo3.modelo.modificadores.multiplicadores.MultiplicadorJugador;
+import javafx.geometry.Pos;
+import javafx.scene.layout.HBox;
+
+import java.util.ArrayList;
+
+public class CajaBonusJugador extends HBox {
+
+    private static int ESPACIADO_BONUS = 5;
+
+    public CajaBonusJugador(Jugador jugador) {
+        super(ESPACIADO_BONUS);
+        super.setAlignment(Pos.CENTER);
+        ArrayList<MultiplicadorJugador> multiplicadores = jugador.multiplicadores();
+        crearCirculosMultiplicador(multiplicadores);
+        ArrayList<Exclusividad> exclusividades = jugador.exclusividades();
+        crearCirculosExclusividad(exclusividades);
+    }
+
+    private void crearCirculosExclusividad(ArrayList<Exclusividad> exclusividades){
+        for (Exclusividad exclusividad:exclusividades) {
+            super.getChildren().add(new CirculoBonus(exclusividad));
+        }
+    }
+
+    private void crearCirculosMultiplicador(ArrayList<MultiplicadorJugador> multiplicadores){
+        for (MultiplicadorJugador multiplicador:multiplicadores) {
+            super.getChildren().add(new CirculoBonus(multiplicador));
+        }
+
+    }
+}
